469fea142034cb8fdb6750792a08834edc38e8f4
sanderdan <sander.danielsson@gmail.com>
2016-11-07 20:42:42 +0100

    Add password promt after wake up.

--- osxdefaults.sh
+++ osxdefaults.sh
@@ -1,3 +1,6 @@
 # Screen: save screenshots to the desktop
 defaults write com.apple.screencapture location -string "$HOME/Desktop"
 
+# Screen: require password immediately after sleep or screen saver begins
+defaults write com.apple.screensaver askForPassword -int 1
+defaults write com.apple.screensaver askForPasswordDelay -int 0
